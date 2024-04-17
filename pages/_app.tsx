9e54d4c30a06ec5731ee27dfa26070bf284862cd
Matteo <matteo.courquin@fortuneo.com>
2024-04-17 15:36:33 +0200

    :bug: Fix .includes /studio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,24 @@ import SmoothScrolling from '@/layout/lenis';
 import '@/styles/main.scss';
 import type { AppProps } from 'next/app';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const pathname = usePathname();
+  const [layoutEnabled, setLayoutEnabled] = useState(true);
+
+  useEffect(() => {
+    setLayoutEnabled(!pathname?.includes('studio'));
+  }, [pathname]);
+
   return (
     <SmoothScrolling>
-      {usePathname().includes('studio') ? (
-        <Component {...pageProps} />
-      ) : (
+      {layoutEnabled ? (
         <Layout>
           <Component {...pageProps} />
         </Layout>
+      ) : (
+        <Component {...pageProps} />
       )}
     </SmoothScrolling>
   );
